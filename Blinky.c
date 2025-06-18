cba46df4b2806b4637195d0bb19702135b81346f
John Honniball <coredump@virginmedia.com>
2025-06-18 23:44:53 +0100

    Initialise MCU clocks for 120MHz operation.
    Divide 48MHz oscillator down to 1MHz, then multiply back up to 120MHz. Dally loops only about 1.5 times faster due to wait states.

--- Blinky.c
+++ Blinky.c
@@ -4,6 +4,93 @@
 #include <sam.h>
 
 
+/* initMCU --- initialise the microcontroller in general */
+
+static void initMCU(void)
+{
+    // Set up wait states
+    NVMCTRL_REGS->NVMCTRL_CTRLA = NVMCTRL_CTRLA_RWS(0) | NVMCTRL_CTRLA_AUTOWS_Msk;
+    
+    /* GCLK2 initialisation: divide DFLL48 by 48 to get 1MHz */
+    GCLK_REGS->GCLK_GENCTRL[2] = GCLK_GENCTRL_DIV(48) | GCLK_GENCTRL_SRC_DFLL | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK2) == GCLK_SYNCBUSY_GENCTRL_GCLK2)
+    {
+        /* Wait for the Generator 2 synchronisation */
+    }
+    
+    /* Connect GCLK to GCLK2 (1MHz) */
+    GCLK_REGS->GCLK_PCHCTRL[1] = GCLK_PCHCTRL_GEN(0x2)  | GCLK_PCHCTRL_CHEN_Msk;
+    while ((GCLK_REGS->GCLK_PCHCTRL[1] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+    {
+        /* Wait for synchronisation */
+    }
+    
+    /* Configure DPLL    */
+    // FILTER = 0
+    // LTIME = 0    No time-out
+    // REFCLK = 0   GCLK (1MHz)
+    // DIV = 0
+    OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLCTRLB = OSCCTRL_DPLLCTRLB_FILTER(0) | OSCCTRL_DPLLCTRLB_LTIME(0x0)| OSCCTRL_DPLLCTRLB_REFCLK(0);
+
+    // LDRFRAC = 0
+    // LDR = 119    Divide-by-120
+    OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLRATIO = OSCCTRL_DPLLRATIO_LDRFRAC(0) | OSCCTRL_DPLLRATIO_LDR(119);
+
+    while((OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLSYNCBUSY & OSCCTRL_DPLLSYNCBUSY_DPLLRATIO_Msk) == OSCCTRL_DPLLSYNCBUSY_DPLLRATIO_Msk)
+    {
+        /* Wait for synchronisation */
+    }
+    
+    /* Enable DPLL */
+    OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLCTRLA = OSCCTRL_DPLLCTRLA_ENABLE_Msk;
+
+    while((OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLSYNCBUSY & OSCCTRL_DPLLSYNCBUSY_ENABLE_Msk) == OSCCTRL_DPLLSYNCBUSY_ENABLE_Msk)
+    {
+        /* Wait for DPLL enable synchronisation */
+    }
+
+    while((OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLSTATUS & (OSCCTRL_DPLLSTATUS_LOCK_Msk | OSCCTRL_DPLLSTATUS_CLKRDY_Msk)) !=
+                (OSCCTRL_DPLLSTATUS_LOCK_Msk | OSCCTRL_DPLLSTATUS_CLKRDY_Msk))
+    {
+        /* Wait for Ready state */
+    }
+    
+    /* Initialise CPU clock */
+    // CPUDIV = 1           Divide-by-1
+    MCLK_REGS->MCLK_CPUDIV = MCLK_CPUDIV_DIV(0x01);
+
+    while((MCLK_REGS->MCLK_INTFLAG & MCLK_INTFLAG_CKRDY_Msk) != MCLK_INTFLAG_CKRDY_Msk)
+    {
+        /* Wait for Main Clock to be Ready */
+    }
+    
+    /* Initialise GCLK0 */
+    // GENCTRL0.DIV = 1     Divide-by-1
+    // GENCTRL0.DIVSEL = 0  Divide-by-GENTRL0.DIV
+    // GENCTRL0.SRC = 1     DPLL0 (120MHz)
+    // GENCTRL0.GENEN = 1   Enabled
+    GCLK_REGS->GCLK_GENCTRL[0] = GCLK_GENCTRL_DIV(1) | GCLK_GENCTRL_SRC_DPLL0 | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK0) == GCLK_SYNCBUSY_GENCTRL_GCLK0)
+    {
+        /* Wait for Generator 0 synchronisation */
+    }
+    
+    /* GCLK1 initialisation DFLL (48MHz) divide-by-1 */
+    // GENCTRL1.DIV = 1     Divide-by-1
+    // GENCTRL1.DIVSEL = 0  Divide-by-GENTRL1.DIV
+    // GENCTRL1.SRC = 1     DFLL
+    // GENCTRL1.GENEN = 1   Enabled
+    GCLK_REGS->GCLK_GENCTRL[1] = GCLK_GENCTRL_DIV(1) | GCLK_GENCTRL_SRC_DFLL | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK1) == GCLK_SYNCBUSY_GENCTRL_GCLK1)
+    {
+        /* Wait for Generator 1 synchronisation */
+    }
+}
+
+
 void oneLedOn(const int led)
 {
     PORT_REGS->GROUP[0].PORT_OUTCLR = PORT_PA00;
@@ -53,6 +140,8 @@ void main(void)
 {
     volatile int dally;
     int i;
+    
+    initMCU();
     
     PORT_REGS->GROUP[0].PORT_DIRSET = PORT_PA00;    // LED pins to outputs
     PORT_REGS->GROUP[1].PORT_DIRSET = PORT_PB30;
