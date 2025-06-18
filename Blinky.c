7b7c21a5041cdfd8eb659fb3b3653ab72430e29f
John Honniball <coredump@virginmedia.com>
2025-06-19 00:12:50 +0100

    Introduce 1ms/1000Hz timer.
    Arduino-like function 'millis()' to access millisecond counter. Eliminate pesky dally loops.

--- Blinky.c
+++ Blinky.c
@@ -2,6 +2,67 @@
 
 
 #include <sam.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+
+volatile uint32_t Milliseconds = 0L;
+volatile bool Tick = false;
+
+
+void TC3_Handler(void)
+{
+    Milliseconds++;
+    Tick = true;
+    
+    //PORT_REGS->GROUP[1].PORT_OUTTGL = PORT_PB06;    // 500Hz on PB06
+    //PORT_REGS->GROUP[0].PORT_OUTTGL = PORT_PA12;    // 500Hz on PA12
+
+    // Acknowledge interrupt
+    TC3_REGS->COUNT16.TC_INTFLAG |= TC_INTFLAG_MC0(1);
+}
+
+
+/* millis --- return milliseconds since reset */
+
+uint32_t millis(void)
+{
+    return (Milliseconds);
+}
+
+
+/* initMillisecondTimer --- set up a timer to interrupt every millisecond */
+
+static void initMillisecondTimer(void)
+{
+    // Connect GCLK1 to TC3
+    GCLK_REGS->GCLK_PCHCTRL[26] = GCLK_PCHCTRL_GEN(0x1U) | GCLK_PCHCTRL_CHEN_Msk;
+
+    while ((GCLK_REGS->GCLK_PCHCTRL[26] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+        ;
+    
+    // Main Clock setup to supply clock to TC3
+    MCLK_REGS->MCLK_APBBMASK |= MCLK_APBBMASK_TC3(1);
+    
+    // Set up TC3 for regular 1ms interrupt
+    TC3_REGS->COUNT16.TC_CTRLA = TC_CTRLA_MODE_COUNT16 | TC_CTRLA_PRESCALER_DIV64;
+    TC3_REGS->COUNT16.TC_COUNT = 0;
+    TC3_REGS->COUNT16.TC_WAVE = TC_WAVE_WAVEGEN_MFRQ;
+    TC3_REGS->COUNT16.TC_CC[0] = ((48000000 / 64) / 1000) - 1;
+    TC3_REGS->COUNT16.TC_INTENSET = TC_INTENSET_MC0(1);
+    
+    /* Set TC3 Interrupt Priority to Level 3 */
+    NVIC_SetPriority(TC3_IRQn, 3);
+    
+    /* Enable TC3 NVIC Interrupt Line */
+    NVIC_EnableIRQ(TC3_IRQn);
+    
+    TC3_REGS->COUNT16.TC_CTRLA |= TC_CTRLA_ENABLE(1);
+    
+    // Wait until TC3 is enabled
+    //while(TC3->COUNT16.STATUS.bit.SYNCBUSY == 1)
+    //    ;
+}
 
 
 /* initMCU --- initialise the microcontroller in general */
@@ -138,10 +199,11 @@ void oneLedOn(const int led)
 
 void main(void)
 {
-    volatile int dally;
+    uint32_t t;
     int i;
     
     initMCU();
+    initMillisecondTimer();
     
     PORT_REGS->GROUP[0].PORT_DIRSET = PORT_PA00;    // LED pins to outputs
     PORT_REGS->GROUP[1].PORT_DIRSET = PORT_PB30;
@@ -159,7 +221,8 @@ void main(void)
         {
             oneLedOn(i);
             
-            for (dally = 0; dally < 500000; dally++)
+            t = millis();
+            while((millis() - t) < 200)
                 ;
         }
         
@@ -167,7 +230,8 @@ void main(void)
         {
             oneLedOn(i);
             
-            for (dally = 0; dally < 500000; dally++)
+            t = millis();
+            while((millis() - t) < 200)
                 ;
         }
     }
